65ddf75bff5daa3433518f111844c6873e063746
jarred <jarredbishop@gmail.com>
2018-06-23 15:37:45 +1200

    remove console.log statemenmt

--- src/components/analytics.js
+++ src/components/analytics.js
@@ -5,7 +5,6 @@ class Analytics extends React.Component {
   componentDidMount() {
     ReactGA.initialize('UA-71711569-9')
     ReactGA.pageview(window.location.pathname + window.location.search)
-    console.log('analytics')
   }
   render() {
     return null
