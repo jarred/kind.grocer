bce20a92cb134d9012284a09d02ad63d15602ff6
jarred <jarredbishop@gmail.com>
2018-09-25 16:49:10 +1200

    chef page

---
+++ src/pages/chef.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <p>
+      We are looking for an innovative and experienced chef to join our company.
+    </p>
+
+    <div className="cta">
+      <a href="/pdfs/kind-grocer-head-chef.pdf">Download Job Description</a>
+    </div>
+  </div>
+)
+
+export default IndexPage
