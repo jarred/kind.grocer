84343c9416ef2c3a2f997a4251c90af98e020826
jarred <jarredbishop@gmail.com>
2019-01-30 09:29:26 +1300

    update and add address

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,15 @@ import SubscribeForm from '../components/subscribe-form'
 const IndexPage = () => (
   <div>
     <p>
-      A modern plant-based grocer, deli and herbal dispensary coming soon to
-      Dunedin's Warehouse Precinct, New Zealand.
+      A modern plant-based grocer, deli and herbal dispensary in Dunedin's
+      Warehouse Precinct, New Zealand.
     </p>
 
     <div className="cta">
+      <a href="https://goo.gl/maps/hxqGQSXfXBt">
+        49 Vogel St, <br />Dunedin, 9016
+      </a>
+      <br /> <br />
       <a href="http://instagram.com/kind.grocer">Follow us on Instagram</a>
       <br />
       <a href="https://confirmsubscription.com/h/r/D3D4DF93AF45704A">
