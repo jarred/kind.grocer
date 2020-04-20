eee5555a0cb860c67844f52a67ccf4e8b41a42d0
Jarred Bishop <jarredbishop@gmail.com>
2020-04-20 18:54:01 +1200

    cleanup + mv _redirects

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Kind.grocer',
-    description:
-      'A modern, plant-based grocer & deli coming soon to Vogel St. Dunedin.',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
